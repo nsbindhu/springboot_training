da382f6a37459505156dd3235720fd2eca7bd28b
Admin <Admin@Admin-PC>
2018-10-12 16:20:06 +0530

    12-10 changes

--- emp-data-service/src/main/java/com/demo/spring/EmpDataServiceApplication.java
+++ emp-data-service/src/main/java/com/demo/spring/EmpDataServiceApplication.java
@@ -2,6 +2,7 @@ package com.demo.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -14,6 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableEurekaClient
 public class EmpDataServiceApplication {
 	
 	public static void main(String[] args) {
